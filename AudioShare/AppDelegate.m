dff75a449a99acc5402a82c023c39360a3d2e68b
lanou3g <lanou3g@zhaoxinlei.local>
2015-08-13 19:06:45 +0800

    review1

--- AudioShare/AppDelegate.m
+++ AudioShare/AppDelegate.m
@@ -333,7 +333,9 @@
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     PlayerViewController *playerVC = [PlayerViewController sharedPlayer];
     if (playerVC.isPlaying) {
-        [playerVC.player play];
+        if (playerVC.foreground) {
+            [playerVC.player play];
+        }     
     }
 }
 
