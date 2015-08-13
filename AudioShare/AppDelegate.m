bfc1171e5cbc4cfb5cb9cc1afd877f42313e612d
lanou3g <lanou3g@zhaoxinlei.local>
2015-08-13 15:35:45 +0800

    commit 后台播放

--- AudioShare/AppDelegate.m
+++ AudioShare/AppDelegate.m
@@ -261,11 +261,6 @@
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     
-    PlayerViewController *playerVC = [PlayerViewController sharedPlayer];
-    DLog(@"call -=-- timer-%@", playerVC.timer);
-    if (playerVC.isPlaying) {
-        [playerVC.player pause];
-    }
     
 }
 
